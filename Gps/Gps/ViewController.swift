af9ac0af7ce3551cf1c847b8c6c9468c5ec6b6e9
nthomas11 <nthomas@sfu.ca>
2017-03-04 20:33:04 -0800

    Update ViewController.swift
    added gps coordinates of restaurants

--- Gps/Gps/ViewController.swift
+++ Gps/Gps/ViewController.swift
@@ -69,10 +69,36 @@ class ViewController: UIViewController, MKMapViewDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // Restaurants
-        names = ["Bamboo Garden", "Big Smoke Burger", "Bubble World", "Club Ilia", "Donair Town", "Gawon Korean", "Guadalupe Handmade Burritos", "Higher Grounds Coffee Shop", "Highland Pub", "Ichibankan Express SFU", "Nature Garden", "Nester's Market", "Pizza Hut", "Plum GAden Noodle House", "Poke Bar", "Quesada Burritos and Tacos", "Renaisance Coffee", "Spicy STone", "Starbucks Cornerstone", "Starbucks Westmall", "Subyway COrnerStone", "Subway Mackenzie CAfe", "Tim Hortons", "Triple O's", "Jugo Juice" ]
+        names = ["Bamboo Garden", "Big Smoke Burger", "Bubble World", "Club Ilia", "Donair Town", "Gawon Korean", "Guadalupe Handmade Burritos", "Higher Grounds Coffee Shop", "Highland Pub", "Ichibankan Express SFU", "Nature Garden", "Nester's Market", "Pizza Hut", "Plum GAden Noodle House", "Poke Bar", "Quesada Burritos and Tacos", "Renaisance Coffee", "Spicy STone", "Starbucks Cornerstone", "Starbucks Westmall", "Subyway COrnerStone", "Subway Mackenzie CAfe", "Tim Hortons", "Triple O's", "Jugo Juice", "Mackenzie Cafe" ]
         
         
-        // Latitudes, Longitudes coordinates of each restaurant  = [
+        // Latitudes, Longitudes coordinates of each restaurant  
+        coordinates = [[49.277839, -122.912081],
+            [49.277867, -122.912372],
+            [49.278819, -122.919041],
+            [49.277986, -122.912335],
+            [49.277827, -122.911922],
+            [49.278578, -122.918853],
+            [49.278602, -122.918732],
+            [49.278887, -122.918655],
+            [49.278618, -122.919001],
+            [49.27811, -122.912069],
+            [49.278012, -122.911477],
+            [49.277797, -122.910073],
+            [49.277842, -122.911995],
+            [49.277913, -122.911802],
+            [49.2778, -122.911854],
+            [49.277778, -122.911582],
+            [49.279397, -122.915624],
+            [49.277829, -122.912029],
+            [49.278124, -122.91228],
+            [49.2796, -122.920961],
+            [49.278061, -122.911894],
+            [49.278868, -122.915608],
+            [49.280247, -122.922318],
+            [49.279311, -122.91712],
+            [49.279412, -122.915741],
+            [49.278868, -122.915608]
             
         ]
         
