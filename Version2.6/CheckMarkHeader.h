f3268a5c164fbdcb5ed6316b56157eaa41b588a1
Grazietta Hof <graziettahof@gmail.com>
2017-04-02 16:25:30 -0700

    ...

--- Version2.6/CheckMarkHeader.h
+++
@@ -1,9 +0,0 @@
-//
-//  CheckMarkHeader.h
-//  restaurantlist
-//
-//  Created by Grazietta Hof on 2017-03-22.
-//  Copyright © 2017 Grazietta Hof. All rights reserved.
-//
-#import "BEMCheckBox.h"
-
