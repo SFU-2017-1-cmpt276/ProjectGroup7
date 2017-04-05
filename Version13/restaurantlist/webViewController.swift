cd6410a546d15f1097ada04ecc7c49396ff51008
Fran Ramunno <franramunno@gmail.com>
2017-04-05 11:45:39 -0700

    Update webViewController.swift

--- Version13/restaurantlist/webViewController.swift
+++ Version13/restaurantlist/webViewController.swift
@@ -1,18 +1,22 @@
 
-//
-//  webViewController.swift
-//  restaurantlist
-//
-//  Created by Grazietta Hof on 2017-03-03.
-//  Copyright © 2017 Grazietta Hof. All rights reserved.
-//
-//Programmers: Fran, Grazietta,Nicole, Jordan
-//coding standard:
-//The names of variables and items should be self-descriptive
-//if any changes are made to existing code in this file notify all programmers in the group chat
-//Xcode bracket convention should be followed
-//only when the updates you have made are compiling, may this projected be submitted to the master branch on github
-//notify all programmers of any existing bugs in the compiling version of the project submitted on the github master branch
+/*
+
+  ViewController.swift
+  restaurantlist
+
+  Created by Grazietta Hof on 2017-03-03.
+  Copyright © 2017 Grazietta Hof. All rights reserved.
+
+  PROGRAMMERS: Fran, Grazietta, Nicole, Jordan
+
+  CODING STANDARD:
+  * The names of variables and items should be self-descriptive
+  * If any changes are made to existing code in this file notify all programmers in the group chat
+  * Xcode bracket convention should be followed
+  * Only when the updates you have made are compiling, may this projected be submitted to the master branch on github
+  * Notify all programmers of any existing bugs in the compiling version of the project submitted on the github master branch
+
+*/
 
 
 import UIKit
