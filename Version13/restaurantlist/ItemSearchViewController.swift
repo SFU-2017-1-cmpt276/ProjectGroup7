ca31db4b30dcefbedd73d40b3809595495286a09
Fran Ramunno <franramunno@gmail.com>
2017-04-05 11:29:38 -0700

    Add comments

--- Version13/restaurantlist/ItemSearchViewController.swift
+++ Version13/restaurantlist/ItemSearchViewController.swift
@@ -1,17 +1,23 @@
-//
-//  ItemSearchViewController.swift
-//  restaurantlist
-//
-//  Created by Grazietta Hof on 2017-03-25.
-//  Copyright © 2017 Grazietta Hof. All rights reserved.
-//
-//Programmers: Fran, Grazietta,Nicole, Jordan
-//coding standard:
-//The names of variables and items should be self-descriptive
-//if any changes are made to existing code in this file notify all programmers in the group chat
-//Xcode bracket convention should be followed
-//only when the updates you have made are compiling, may this projected be submitted to the master branch on github
-//notify all programmers of any existing bugs in the compiling version of the project submitted on the github master branch
+
+/*
+
+  ItemSearchViewController.swift
+  restaurantList
+
+  Created by Grazietta Hof on 2017-03-25.
+  Copyright © 2017 Grazietta Hof. All rights reserved.
+
+  PROGRAMMERS: Fran, Grazietta, Nicole, Jordan
+
+  CODING STANDARD:
+  * The names of variables and items should be self-descriptive
+  * If any changes are made to existing code in this file notify all programmers in the group chat
+  * Xcode bracket convention should be followed
+  * Only when the updates you have made are compiling, may this projected be submitted to the master branch on github
+  * Notify all programmers of any existing bugs in the compiling version of the project submitted on the github master branch
+
+*/
+
 import UIKit
 
 class ItemSearchViewController: UITableViewController{
@@ -26,133 +32,143 @@ class ItemSearchViewController: UITableViewController{
     //initialize the filtered array for search functionality
   
  
-override func viewDidLoad() {
+    override func viewDidLoad() {
 
-      // Setup the Search Controller
-        searchController.searchResultsUpdater = self
-        searchController.searchBar.delegate = self
-        definesPresentationContext = true
-        searchController.dimsBackgroundDuringPresentation = false
-        tableView.tableHeaderView = searchController.searchBar
-    }
+            // Setup the Search Controller
+            searchController.searchResultsUpdater = self
+            searchController.searchBar.delegate = self
+            definesPresentationContext = true
+            searchController.dimsBackgroundDuringPresentation = false
+            tableView.tableHeaderView = searchController.searchBar
+        }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
     }
     
+    /**************************************************************************************************/
+    //          This function returns the number of rows to be loaded into the tableView              //
+    /**************************************************************************************************/
     
-//this function returns the number of rows to be loaded into the tableview
-override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     
-        if searchController.isActive && searchController.searchBar.text != "" {
-            return self.filteredData.count
-        
-        }
-        else{
-            return data.count
+            if searchController.isActive && searchController.searchBar.text != "" {
+                return self.filteredData.count
+
+            }
+
+            else {
+                return data.count
             }
     }
     
-func filterContentForSearchText(_ searchText: String) {
-        filteredData = data.filter({( object : ItemListClass) -> Bool in
-        return object.Name.lowercased().contains(searchText.lowercased())
-        })
-        tableView.reloadData()
-    }
+    func filterContentForSearchText(_ searchText: String) {
+            filteredData = data.filter({( object : ItemListClass) -> Bool in
+            return object.Name.lowercased().contains(searchText.lowercased())
+            })
+            tableView.reloadData()
+     }
 
+    /**************************************************************************************************/
+    // This function loads the data into the cells.                                                   //
+    //     * If the flagTotal is set to 1, then at least one of the checkMarks are selected           //
+    //     * and a different array is used                                                            //
+    //     * Return the cell                                                                          //
+    /**************************************************************************************************/
 
-//This function loads the data into the cells. If the flagTotal is set to 1, then at least one of the checkMarks are selected and a different array is used. It returns the cell
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
-override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    
-    let cell = tableView.dequeueReusableCell(withIdentifier: "cell3", for: indexPath) as! ButtonCell
-    
-    if searchController.isActive && searchController.searchBar.text != ""{
-        cell.rowLabel2.text = filteredData[indexPath.row].Name
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell3", for: indexPath) as! ButtonCell
+
+        if searchController.isActive && searchController.searchBar.text != ""{
+            cell.rowLabel2.text = filteredData[indexPath.row].Name
+            cell.tapAction = {
+                (cell) in self.showAlertForRow(tableView.indexPath(for: cell)!.row)
+            }
+            return cell
+        }
+
+        cell.rowLabel2.text = data[indexPath.row].Name
         cell.tapAction = {
             (cell) in self.showAlertForRow(tableView.indexPath(for: cell)!.row)
         }
         return cell
     }
     
-    cell.rowLabel2.text = data[indexPath.row].Name
-    cell.tapAction = {
-        (cell) in self.showAlertForRow(tableView.indexPath(for: cell)!.row)
-    }
-    return cell
-}
-
-//This function is responsible for the item detail pop up
-func showAlertForRow(_ row: Int) {
+    /**************************************************************************************************/
+    //                     This function is responsible for the item detail pop up                    //
+    /**************************************************************************************************/
     
-    if searchController.isActive && searchController.searchBar.text != ""{
-        if filteredData[row].Restuarant == "PizzaHut"{
-            let alert = UIAlertController(
-            title: filteredData[row].Restuarant + "\n" + filteredData[row].Name,
-            message: "Calories:  \(filteredData[row].Calories)/slice\nFats:  \(filteredData[row].Fats)g\nCarbs:  \(filteredData[row].Carbs)g\nProtein:  \(filteredData[row].Protein)g\nSodium:  \(filteredData[row].Sodium)mg",
-            preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Gotcha!", style: UIAlertActionStyle.default, handler: { (test) -> Void in
-            //self.dismiss(animated: true, completion: nil)
-        }))
-        
-        self.present(
-            alert,
-            animated: true,
-            completion: nil)
-        }
-        
-        else{
-            let alert = UIAlertController(
+    func showAlertForRow(_ row: Int) {
+
+        if searchController.isActive && searchController.searchBar.text != ""{
+            if filteredData[row].Restuarant == "PizzaHut"{
+                let alert = UIAlertController(
                 title: filteredData[row].Restuarant + "\n" + filteredData[row].Name,
-                message: "Calories:  \(filteredData[row].Calories)\nFats:  \(filteredData[row].Fats)g\nCarbs:  \(filteredData[row].Carbs)g\nProtein:  \(filteredData[row].Protein)g\nSodium:  \(filteredData[row].Sodium)mg",
+                message: "Calories:  \(filteredData[row].Calories)/slice\nFats:  \(filteredData[row].Fats)g\nCarbs:  \(filteredData[row].Carbs)g\nProtein:  \(filteredData[row].Protein)g\nSodium:  \(filteredData[row].Sodium)mg",
                 preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "Gotcha!", style: UIAlertActionStyle.default, handler: { (test) -> Void in
                 //self.dismiss(animated: true, completion: nil)
             }))
-            
+
             self.present(
                 alert,
                 animated: true,
                 completion: nil)
             }
-        }
-        
-        
-    else{
-        if data[row].Restuarant == "PizzaHut"{
-        let alert = UIAlertController(
-            title: data[row].Restuarant + "\n" + data[row].Name,
-            message: "Calories:  \(data[row].Calories)/slice\nFats:  \(data[row].Fats)g\nCarbs:  \(data[row].Carbs)g\nProtein:  \(data[row].Protein)g\nSodium:  \(data[row].Sodium)mg",
-            preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Gotcha!", style: UIAlertActionStyle.default, handler: { (test) -> Void in
-            self.dismiss(animated: true, completion: nil)
-        }))
-        
-        self.present(
-            alert,
-            animated: true,
-            completion: nil)
-        }
-        
+
+            else{
+                let alert = UIAlertController(
+                    title: filteredData[row].Restuarant + "\n" + filteredData[row].Name,
+                    message: "Calories:  \(filteredData[row].Calories)\nFats:  \(filteredData[row].Fats)g\nCarbs:  \(filteredData[row].Carbs)g\nProtein:  \(filteredData[row].Protein)g\nSodium:  \(filteredData[row].Sodium)mg",
+                    preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Gotcha!", style: UIAlertActionStyle.default, handler: { (test) -> Void in
+                    //self.dismiss(animated: true, completion: nil)
+                }))
+
+                self.present(
+                    alert,
+                    animated: true,
+                    completion: nil)
+                }
+            }
+
+
         else{
+            if data[row].Restuarant == "PizzaHut"{
             let alert = UIAlertController(
                 title: data[row].Restuarant + "\n" + data[row].Name,
-                message: "Calories:  \(data[row].Calories)\nFats:  \(data[row].Fats)g\nCarbs:  \(data[row].Carbs)g\nProtein:  \(data[row].Protein)g\nSodium:  \(data[row].Sodium)mg",
+                message: "Calories:  \(data[row].Calories)/slice\nFats:  \(data[row].Fats)g\nCarbs:  \(data[row].Carbs)g\nProtein:  \(data[row].Protein)g\nSodium:  \(data[row].Sodium)mg",
                 preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "Gotcha!", style: UIAlertActionStyle.default, handler: { (test) -> Void in
                 self.dismiss(animated: true, completion: nil)
             }))
-            
+
             self.present(
                 alert,
                 animated: true,
                 completion: nil)
-            
             }
-        }
-    
-    }
+
+            else{
+                let alert = UIAlertController(
+                    title: data[row].Restuarant + "\n" + data[row].Name,
+                    message: "Calories:  \(data[row].Calories)\nFats:  \(data[row].Fats)g\nCarbs:  \(data[row].Carbs)g\nProtein:  \(data[row].Protein)g\nSodium:  \(data[row].Sodium)mg",
+                    preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Gotcha!", style: UIAlertActionStyle.default, handler: { (test) -> Void in
+                    self.dismiss(animated: true, completion: nil)
+                }))
+
+                self.present(
+                    alert,
+                    animated: true,
+                    completion: nil)
+
+                }
+            }
+
+     }
     
 }
 
@@ -171,7 +187,7 @@ extension ItemSearchViewController: UISearchResultsUpdating {
         
         filterContentForSearchText(searchController.searchBar.text!)
     }
- }
+}
  
 
 
